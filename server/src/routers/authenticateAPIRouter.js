c25f260f9ecce32ae079ad53ced282c1431fe787
myramyra-lab <71420810+myramyra-lab@users.noreply.github.com>
2022-07-23 23:55:18 +0300

    Update authenticateAPIRouter.js

--- server/src/routers/authenticateAPIRouter.js
+++ server/src/routers/authenticateAPIRouter.js
@@ -21,22 +21,6 @@ router.get("/keys",authenticationController.getAll)
 // router.get("/items", API.validateKey,authenticationController.getItems)
 router.get("/items", authenticationController.getItems)
 
-
-
-//checkJwt is an Express Middleware that will validate access tokens 
-// const checkJwt = jwt({
-//   secret:jwksRsa.expressJwtSecret({
-//     cache:true,
-//     rateLimit:true,
-//     jwksRequestsPerMinute:5,
-//     jwksUri:`https://dev-z71ownnb.us.auth0.com/.well-known/jwks.json`
-//   }),
-//   audience:'https://apiauth-aggie',
-//   issue:`https://dev-z71ownnb.us.auth0.com/api/v2/`,
-//   algorithms:['RS256']
-// })
-// app.use(checkJwt)
-//the checkJwt middleware will intercept post requests only
 router.post("/createitem", authenticationController.createItem)
 router.post("/register",authenticationController.create)
 
